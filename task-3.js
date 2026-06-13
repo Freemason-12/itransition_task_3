95ce96b660af335ac5925be709e8d5a3f1dbef1d
Freemason-12 <maksatlego@gmail.com>
2026-06-13 15:28:21 +0500

    fixed usecases when large numbers are used

--- task-3.js
+++ task-3.js
@@ -3,16 +3,19 @@ import http from 'node:http';
 const server = http.createServer(function(request, response) {
   const tail = request.url.split('/').pop();
   if (tail.includes('maksatlego_gmail_com')) {
-    const queryParams = tail.split('?').pop().split('&').map((p) => Number(p.split('=').pop()))
+    const queryParams = tail.split('?').pop().split('&').map((p) => BigInt(Number(p.split('=').pop())))
     const x = queryParams[0], y = queryParams[1];
-    if (x <= 0 || y <= 0 || x % 1 !== 0 || y % 1 !== 0) response.end('NaN');
+    if (x <= 0n || y <= 0n || x % 1n !== 0n || y % 1n !== 0n) response.end('NaN');
     else {
       let lcm = x;
-      while (lcm % y !== 0) lcm += x;
+      while (lcm % y !== 0n) lcm += x;
       response.end(lcm.toString());
     }
   }
-  else response.end('please navigate to appropriate url (404 not found)\n');
+  else {
+    response.writeHead(404, 'Not found');
+    response.end('please navigate to appropriate url (404 not found)\n');
+  }
 });
 
 server.listen(8000);
