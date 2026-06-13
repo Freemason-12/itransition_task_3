107a7f2fc45fce936efebf2be445ee6e9d9b857a
Freemason-12 <maksatlego@gmail.com>
2026-06-13 14:23:24 +0500

    hopefully finished the task

--- task-3.js
+++ task-3.js
@@ -1,7 +1,15 @@
 import http from 'node:http';
 
 const server = http.createServer(function(request, response) {
-  response.end('hello world');
+  const tail = request.url.split('/').pop();
+  if (tail.includes('maksatlego_gmail_com')) {
+    const queryParams = tail.split('?').pop().split('&').map((p) => Number(p.split('=').pop()))
+    const x = queryParams[0], y = queryParams[1];
+    let lcm = x;
+    while (lcm % y !== 0) lcm += x;
+    response.end(lcm.toString());
+  }
+  else response.end('please navigate to appropriate url (404 not found)\n');
 });
 
 server.listen(8000);
